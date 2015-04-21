0627dfae79ab4f875e3bf778a830e2f5ff5f8724
Varun Garg <gargv@codeaurora.org>
2015-04-21 16:06:56 +0530

    oneplus2: Add property for min/max cpus of core control
    Set the min and max cpu core values of core ctl.
    These properties also help in the crash recovery of perfd.
    
    * Also change core_ctl values to be a bit more power-friendly
    
    Change-Id: I28a76a5b6e17baf0adaeab426b7843b52fff4281

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -112,10 +112,13 @@ case "$target" in
          echo 0:1344000 > /sys/module/cpu_boost/parameters/input_boost_freq
          echo 40 > /sys/module/cpu_boost/parameters/input_boost_ms
          # configure core_ctl module parameters
+         echo 4 > /sys/devices/system/cpu/cpu0/core_ctl/max_cpus
+         echo 2 > /sys/devices/system/cpu/cpu0/core_ctl/min_cpus
+         echo 0 > /sys/devices/system/cpu/cpu0/core_ctl/is_big_cluster
          echo 4 > /sys/devices/system/cpu/cpu4/core_ctl/max_cpus
-         echo 2 > /sys/devices/system/cpu/cpu4/core_ctl/min_cpus
-         echo 60 > /sys/devices/system/cpu/cpu4/core_ctl/busy_up_thres
-         echo 30 > /sys/devices/system/cpu/cpu4/core_ctl/busy_down_thres
+         echo 1 > /sys/devices/system/cpu/cpu4/core_ctl/min_cpus
+         echo 68 > /sys/devices/system/cpu/cpu4/core_ctl/busy_up_thres
+         echo 40 > /sys/devices/system/cpu/cpu4/core_ctl/busy_down_thres
          echo 100 > /sys/devices/system/cpu/cpu4/core_ctl/offline_delay_ms
          echo 1 > /sys/devices/system/cpu/cpu4/core_ctl/is_big_cluster
          echo 4 > /sys/devices/system/cpu/cpu4/core_ctl/task_thres
