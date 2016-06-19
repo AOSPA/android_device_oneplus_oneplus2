9b05cd9a7e37d9fb53a177178d55b7287c60fc52
thecrazyskull <anaskarbila@aospa.co>
2016-06-19 15:09:51 -0400

    oneplus2: disable some broken audio features
    * We need libavenhancements to get this to work properly
    
    Change-Id: I2657749dfd752d6f5c8c7d06f4de65674afd17b2

--- device.mk
+++ device.mk
@@ -189,8 +189,7 @@ PRODUCT_PACKAGES += \
     libOmxVdecHevc \
     libOmxVenc \
     libOmxVidcCommon \
-    libstagefrighthw \
-    libstagefright_soft_flacdec
+    libstagefrighthw
 
 # Power
 PRODUCT_PACKAGES += \
